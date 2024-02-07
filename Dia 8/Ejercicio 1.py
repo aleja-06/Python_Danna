1341d312b7d129cd3cc3a76aeb434c34b6beaedd
danna <dannaflorezgomez2005@gmail.com>
2024-02-07 09:18:21 -0500

    cambios

---
+++ Dia 8/Ejercicio 1.py
@@ -0,0 +1,3 @@
+#1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar ordenados por la fecha de realización,
+#mostrando en primer lugar los pedidos más recientes.
+
